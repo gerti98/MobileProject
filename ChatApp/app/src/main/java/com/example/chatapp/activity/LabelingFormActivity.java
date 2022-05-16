8ae485a38154e05218e65b5f3e314b93ca175651
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2022-05-16 17:45:19 +0200

    Added possibility to listen to audio messages on form page

--- ChatApp/app/src/main/java/com/example/chatapp/activity/LabelingFormActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/activity/LabelingFormActivity.java
@@ -5,12 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.chatapp.RecyclerItemClickListener;
 import com.example.chatapp.connection.FirebaseDbManager;
 import com.example.chatapp.R;
 import com.example.chatapp.adapter.LabelingAdapter;
@@ -23,6 +25,7 @@ import java.util.ArrayList;
 public class LabelingFormActivity extends AppCompatActivity {
 
     String TAG = "LabelingFormActivity";
+    private AppCompatActivity thisActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +36,23 @@ public class LabelingFormActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler_label);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         recyclerView.setAdapter(new LabelingAdapter(this, messageList));
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(this, recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        Message msg = messageList.get(position);
+                        if(!msg.getIsAudio())
+                            return;
+                        String receivedRecFilePath = getExternalCacheDir().getAbsolutePath();
+                        receivedRecFilePath += msg.getFilename();
+                        MediaPlayer mediaPlayer = MediaPlayer.create(thisActivity, Uri.parse(receivedRecFilePath));
+                        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                    }
 
+                    @Override public void onLongItemClick(View view, int position) {
+                        // do whatever
+                    }
+                })
+        );
         Button submitButton = findViewById(R.id.submit_button);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
