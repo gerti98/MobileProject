038155c24076b2882c9a2b5ccf5ccf66f6a0068f
ImBadnick <fuma2@hotmail.it>
2022-05-22 23:43:18 +0200

    Commented the server and model analysis code

--- Heroku_server/app.py
+++ Heroku_server/app.py
@@ -14,35 +14,60 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
+# Loading models, vectorizer and encoder -> The vectorizer and encoder are used for text preprocessing
 svm_model = pickle.load(open("models/svm_model.pkl", 'rb'))
 mlp_model = pickle.load(open("models/mlp_model.pkl", 'rb'))
 vectorizer = pickle.load(open("models/tfid.pkl", 'rb'))
 encoder = pickle.load(open("models/encoder.pkl", 'rb'))
 
+# Root API
 @app.route('/')
 def index():
     return "WebAPP used for emotion recognition"
 
+# Predict text emotion API
 @app.route('/predict_text_emotion', methods=['POST'])
 def predict_text_emotion():
-    request_json = request.json
+    """ 
+    INPUT:  The format of the json in the request is:
+                {
+                    msgs: ['msg_1', 'msg_2', ..., 'msg_n']
+                }
+    OUTPUT: The API returns a json containing the list of emotions based on the list of msgs:
+                [emotion_1, emotion_2, ..., emotion_n]
+    """
+
+    request_json = request.json # Get the request in json format
     print(request_json)
-    list_of_msg = request_json['msgs']
+    list_of_msg = request_json['msgs'] # Get the list of msgs
+
+    # Predictions based on the msgs
     svm_predictions = detect_emotions_with_proba(svm_model, vectorizer, list_of_msg)
     predict_emotion_inv = proba_to_labels(encoder, svm_predictions)
     print(predict_emotion_inv)
-    return jsonify(predict_emotion_inv)
 
+    return jsonify(predict_emotion_inv) # Return the list of emotions predictions
+
+
+# Predict audio emotion API
 @app.route('/predict_voice_emotion', methods=['POST'])
 def predict_voice_emotion():
-    audio_file = request.files['file']
+    """ 
+    INPUT:  The request contains the file audio used for the prediction  (.wav file)
+    
+    OUTPUT: The API returns the a json containing the emotion predicted from the model
+    """
 
-    file_name = str(random.randint(0,100000))
-    file_name_to_convert = file_name + '_toconvert.wav'
-    file_name_wav =  file_name + '.wav'
-    audio_file.save(file_name_to_convert)
-    convert_wavs(file_name_to_convert, file_name_wav)
+    audio_file = request.files['file'] # Get the file
 
+    file_name = str(random.randint(0,100000)) # Generate a random filename
+    file_name_to_convert = file_name + '_toconvert.wav' # Filename of the file that has to be converted
+    file_name_wav =  file_name + '.wav' # Filename of the file after the conversion 
+    audio_file.save(file_name_to_convert) # Save the file to be converted 
+    
+    convert_wavs(file_name_to_convert, file_name_wav) # Convert the file and save it 
+
+    # Prediction based on the audio
     features = extract_feature(file_name_wav, mfcc=True, chroma=True, mel=True)
     prediction = mlp_model.predict(features.reshape(1,-1))[0]
 
@@ -50,10 +75,11 @@ def predict_voice_emotion():
     if prediction == 'happy':
         prediction = "joy"
 
+    # Remove the files generated
     os.remove(file_name_to_convert)
     os.remove(file_name_wav)
 
-    return jsonify(prediction)
+    return jsonify(prediction) # Return the emotion predicted
 
 
 if __name__ == '__main__':  
