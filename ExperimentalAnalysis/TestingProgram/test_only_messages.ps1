a48c2fdded80e6cd934fd3d63271398919fee758
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2022-05-26 23:35:58 +0200

    Added test script

---
+++ ExperimentalAnalysis/TestingProgram/test_only_messages.ps1
@@ -0,0 +1,27 @@
+# 1. Change branch to 'testing_branch' 
+# 2. Link your phone with Wireless debugging and run the app
+# 3. Login from the app with any account
+# 4. Run the script and wait patiently
+
+$i = 1
+$j = 1
+$whole_test_repetitions = 1 
+$single_test_repetitions = 3
+
+while ($j -le $whole_test_repetitions){
+    
+    adb shell dumpsys batterystats --reset
+    while ($i -le $single_test_repetitions){
+        adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
+        timeout 10
+        adb shell input keyevent 4
+        $i++
+    }
+    
+    echo "Finished test #$j"
+    $i = 0
+    adb bugreport results/bugreport_auto_test_only_messages_$j.zip
+    $j++    
+
+}
+
