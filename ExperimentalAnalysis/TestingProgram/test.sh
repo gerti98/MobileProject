55fd27b7682d38f5a9c65f0b7f5a8d989f13477c
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2022-05-27 16:01:08 +0200

    Set up automated experiment for power study

--- ExperimentalAnalysis/TestingProgram/test.sh
+++ ExperimentalAnalysis/TestingProgram/test.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
-# Run with > bash test.sh
+# 1. Change branch to 'testing_branch' 
+# 2. Link your phone with Wireless debugging and run the app
+# 3. Login from the app with any account
+# 4. Run the script  with > bash test.sh
+# 5.wait patiently
 
-START_OUT=1
-END_OUT=1
+
 START_IN=1
-END_IN=3
+END_IN=80
 echo "Countdown"
 
 cd $HOME/Android/Sdk/platform-tools
 
-for (( c=$START_OUT; c<=$END_OUT; c++ ))
-do
-    ./adb shell dumpsys batterystats --reset
-    for (( c_=$START_IN; c_<=$END_IN; c_++ ))
-    do
-        ./adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
-        sleep 10
-        ./adb shell input keyevent 4
-        # echo "$c_"
-    done
 
-    echo "Finished test #$c"
-    ./adb bugreport results/bugreport_auto_test_only_messages_$c.zip
+./adb bugreport results/to_be_deleted.zip
+
+./adb shell dumpsys batterystats --reset
+for (( c_=$START_IN; c_<=$END_IN; c_++ ))
+do
+    ./adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
+    sleep 15
+    ./adb shell input keyevent 4
 done
+
+echo "Finished test ONLY MESSAGES"
+./adb bugreport results/bugreport_auto_test_only_messages.zip
+
+./adb shell dumpsys batterystats --reset
+for (( c_=$START_IN; c_<=$END_IN; c_++ ))
+do
+    ./adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "MHZsUPUgc0OrK8JDpzts1N27neA3"
+    sleep 15
+    ./adb shell input keyevent 4
+done
+
+echo "Finished test ONE AUDIO"
+./adb bugreport results/bugreport_auto_test_one_audio.zip
+
+./adb shell dumpsys batterystats --reset
+for (( c_=$START_IN; c_<=$END_IN; c_++ ))
+do
+    ./adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "p7nq5IXiNGXJ4aSOoD34E2iuKkk1"
+    sleep 15
+    ./adb shell input keyevent 4
+done
+
+echo "Finished test THREE AUDIO"
+./adb bugreport results/bugreport_auto_test_three_messages.zip
--- ExperimentalAnalysis/TestingProgram/test_only_messages.ps1
+++ ExperimentalAnalysis/TestingProgram/test_only_messages.ps1
@@ -4,41 +4,42 @@
 # 4. Run the script and wait patiently
 
 $i = 1
-$single_test_repetitions = 3
+$single_test_repetitions = 80
 
-    
+
+adb bugreport results/to_be_deleted.zip
+
 adb shell dumpsys batterystats --reset
 while ($i -le $single_test_repetitions){
     adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
-    timeout 10
+    timeout 15
     adb shell input keyevent 4
     $i++
 }
 
 echo "Finished test ONLY MESSAGES"
-$i = 0
-adb bugreport results/bugreport_auto_test_only_messages_$j.zip
+$i = 1
+adb bugreport results/bugreport_auto_test_only_messages.zip
 
 adb shell dumpsys batterystats --reset
 while ($i -le $single_test_repetitions){
-    adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
-    timeout 10
+    adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "MHZsUPUgc0OrK8JDpzts1N27neA3"
+    timeout 15
     adb shell input keyevent 4
     $i++
 }
 
-echo "Finished test ONLY MESSAGES"
-$i = 0
-adb bugreport results/bugreport_auto_test_only_messages_$j.zip
+echo "Finished test ONE AUDIO"
+$i = 1
+adb bugreport results/bugreport_auto_test_one_audio.zip
 
 adb shell dumpsys batterystats --reset
 while ($i -le $single_test_repetitions){
-    adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
-    timeout 10
+    adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "p7nq5IXiNGXJ4aSOoD34E2iuKkk1"
+    timeout 15
     adb shell input keyevent 4
     $i++
 }
 
-echo "Finished test ONLY MESSAGES"
-$i = 0
-adb bugreport results/bugreport_auto_test_only_messages_$j.zip
+echo "Finished test THREE AUDIO"
+adb bugreport results/bugreport_auto_three_audio.zip
