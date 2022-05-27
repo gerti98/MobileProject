0833626a521bf8c03264f614d6ad60c55be04bf8
fraie98 <francesco.iemma11@gmail.com>
2022-05-27 14:53:49 +0200

    test.sh updated

--- ExperimentalAnalysis/TestingProgram/test.sh
+++ ExperimentalAnalysis/TestingProgram/test.sh
@@ -7,18 +7,20 @@ END_OUT=1
 START_IN=1
 END_IN=3
 echo "Countdown"
- 
+
+cd $HOME/Android/Sdk/platform-tools
+
 for (( c=$START_OUT; c<=$END_OUT; c++ ))
 do
-    adb shell dumpsys batterystats --reset
+    ./adb shell dumpsys batterystats --reset
     for (( c_=$START_IN; c_<=$END_IN; c_++ ))
     do
-        adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
+        ./adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
         sleep 10
-        adb shell input keyevent 4
+        ./adb shell input keyevent 4
         # echo "$c_"
     done
 
     echo "Finished test #$c"
-    adb bugreport results/bugreport_auto_test_only_messages_$c.zip
+    ./adb bugreport results/bugreport_auto_test_only_messages_$c.zip
 done
