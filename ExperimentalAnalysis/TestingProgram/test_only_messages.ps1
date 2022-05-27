15ec7961424197aca1c955fde8117e8e69c6d99b
Olgerti Xhanej <33934387+gerti98@users.noreply.github.com>
2022-05-27 15:31:57 +0200

    Update test_only_messages.ps1

--- ExperimentalAnalysis/TestingProgram/test_only_messages.ps1
+++ ExperimentalAnalysis/TestingProgram/test_only_messages.ps1
@@ -4,24 +4,41 @@
 # 4. Run the script and wait patiently
 
 $i = 1
-$j = 1
-$whole_test_repetitions = 1 
 $single_test_repetitions = 3
 
-while ($j -le $whole_test_repetitions){
     
-    adb shell dumpsys batterystats --reset
-    while ($i -le $single_test_repetitions){
-        adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
-        timeout 10
-        adb shell input keyevent 4
-        $i++
-    }
-    
-    echo "Finished test #$j"
-    $i = 0
-    adb bugreport results/bugreport_auto_test_only_messages_$j.zip
-    $j++    
-
+adb shell dumpsys batterystats --reset
+while ($i -le $single_test_repetitions){
+    adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
+    timeout 10
+    adb shell input keyevent 4
+    $i++
 }
 
+echo "Finished test ONLY MESSAGES"
+$i = 0
+adb bugreport results/bugreport_auto_test_only_messages_$j.zip
+
+adb shell dumpsys batterystats --reset
+while ($i -le $single_test_repetitions){
+    adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
+    timeout 10
+    adb shell input keyevent 4
+    $i++
+}
+
+echo "Finished test ONLY MESSAGES"
+$i = 0
+adb bugreport results/bugreport_auto_test_only_messages_$j.zip
+
+adb shell dumpsys batterystats --reset
+while ($i -le $single_test_repetitions){
+    adb shell am start -n com.example.chatapp/.activity.ChatActivity --es "chat_user_uid" "AR47NLFVJygxpw5is5ZNP3AJZQ03"
+    timeout 10
+    adb shell input keyevent 4
+    $i++
+}
+
+echo "Finished test ONLY MESSAGES"
+$i = 0
+adb bugreport results/bugreport_auto_test_only_messages_$j.zip
